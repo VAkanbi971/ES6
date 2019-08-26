e8dfb79c617b38b25d503db4a0cba3c552eb6660
Victor Akanbi <vakanbi971@west-mec.org>
2019-08-26 15:06:57 -0700

    Updated Let and const
    We accomplished much

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,22 +1,60 @@
-"use strict"
+let LOG = console.log;
 
-var a = "Test1";
-var b = "Test2";
+const pizza = [
+    {
+        name: "pep",
+        number: 3,
+        price: 5
+    },
+    
+    {
+        name: "mushroom",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mango",
+        number: 3,
+        price: 5
+    }
+    // "pep", "mushroom", "mango", "pineappple"
+];
 
-a => a+=a;
+const [pep, mushroom] = pizza;
+
+// console.log(pizza[0].name);
+
+// console.log(pizza[0]);
+// console.log(typeof pep);
+
+/**
+ * Object Destructuring
+ */
+
+ var pizzaObj = {
+     pepp: "monday",
+     array: [
+         1,
+         2,
+         3
+     ],
+     funFunction: function(name){
+         LOG(`I'm ${name}'s function`);
+     }
+ };
+
+//  LOG(pizzaObj["array"]);
+// pizzaObj.funFunction();
+// const {peppStr,arrayArr, funFunction} = pizzaObj;
+
+// funFunction("Victor");
 
 
-const testVar = () => {
-    const a = 30;
-
-    if (true) {
-        let a = 50;
-        console.log(a);
-        a = 30;
+var dogType = "pug";
+var dog = {
+    [dogType + 'Bark'] (){
+        LOG("I'm a pug!");
     }
 }
-
-
-console.log('other A: ${a}');
-
-testVar();  
+const {pugBark} = dog;
+pugBark();
